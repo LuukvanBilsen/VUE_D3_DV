178c662c0d9e36fcb4631c1822d7190ff0d09304
Luuk van Bilsen <luuk.vanbilsen1@gmail.com>
2023-04-08 21:48:14 +0200

    new todo

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import bomen_data from './../public/bomen.json'
+
 
 const vuetify = createVuetify({
     components,
@@ -18,6 +20,18 @@ const vuetify = createVuetify({
 
 createApp(App)
     .use(store)
+    .use(bomen_data)
     .use(router)
     .use(vuetify)
     .mount('#app')
+
+// https://observablehq.com/@mbostock/walmarts-growth // Trees plant-year, maybe colour == species
+
+// https://observablehq.com/@d3/hexbin-map // Amount in Buurt + median plant year
+// https://observablehq.com/@d3/bubble-map // Alternative
+
+
+// https://observablehq.com/@d3/seamless-zoomable-map-tiles // Zoom fix
+// https://observablehq.com/@d3/smooth-zooming // Zoom fix 2
+// https://observablehq.com/@d3/zoom-to-bounding-box //Zoom
+// https://d3-graph-gallery.com/graph/density2d_hexbin.html
